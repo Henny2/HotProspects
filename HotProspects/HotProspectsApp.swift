24f51b83f9f2d63d705ea2e9b99a5ea1e05e3b29
Henny2 <henrieke.baunack@web.de>
2024-01-31 19:33:09 -0800

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Henrieke Baunack on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
