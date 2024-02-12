263b1a917fe15da0a3fe7e4d9a9e3ca07db6cfa6
Henny2 <henrieke.baunack@web.de>
2024-02-11 20:33:26 -0800

    adding QR code generator for own profile

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -27,7 +27,15 @@ struct ProspectsView: View {
     }
     var body: some View {
         NavigationStack{
-            Text("People: \(prospects.count)")
+            List(prospects){ prospect in
+                VStack(alignment:.leading){
+                    Text(prospect.name)
+                        .font(.headline)
+                    
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
+                }
+            }
                 .navigationTitle(title)
                 .toolbar {
                     Button("Scan", systemImage: "qrcode.viewfinder"){
@@ -35,6 +43,18 @@ struct ProspectsView: View {
                         modelContext.insert(prospect)
                     }
                 }
+        }
+    }
+    
+    init(filter: FilterType){
+        self.filter = filter
+        
+        if filter != .none {
+            let showdContactedOnly = filter == .contacted
+            
+            _prospects = Query(filter: #Predicate{
+                $0.isContacted == showdContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
         }
     }
 }
